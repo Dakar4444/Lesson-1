04001e5ce69b9559f795cfb8e9ce8edfc4b74a89
Stas <maks_maks_96_morozov@mail.ru>
2024-02-20 21:36:38 +0300

    home work #1.2

--- HW_1/myproject/myapp/views.py
+++ HW_1/myproject/myapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
+    logger.info('Index page accessed')
     return HttpResponse("<h1>Main page</h1>")
 
 
 def about(request):
+    logger.info('Visited the "About Me" page')
     return HttpResponse("<h1>About me</h1>")
 
